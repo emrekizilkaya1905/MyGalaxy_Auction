ba110ab0c1e8055b442aaa7adcc19fa01e6a5587
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-02-26 00:34:24 +0100

    Setup the tables and definitions of relationships of tables.

---
+++ MyGalaxy_Auction_Data_Access/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyGalaxy_Auction_Data_Access.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGalaxy_Auction_Data_Access.Context
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	
+	}
+}
--- MyGalaxy_Auction_Data_Access/Domain/Vehicle.cs
+++ MyGalaxy_Auction_Data_Access/Domain/Vehicle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyGalaxy_Auction_Data_Access.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -17,13 +18,15 @@ namespace MyGalaxy_Auction_Data_Access.Domain
 		public decimal EngineCapacity { get; set; }
 		public decimal Price { get; set; }
 		public int Millage { get; set; }
-		public string PlateNumber { get;set; }
+		public string? PlateNumber { get;set; }
 		public double AuctionPrice { get; set; }
-		public string AdditionalInformation { get; set; }
+		public string? AdditionalInformation { get; set; }
 		public DateTime StartTime { get; set; }
 		public DateTime EndTime { get; set; }
 		public bool IsActive { get; set; }
-		public string Image { get; set; }
-		public string SellerId { get; set; }
+		public string? Image { get; set; }
+		public string? SellerId { get; set; }
+		public ApplicationUser Seller { get; set; }
+		public virtual List<Bid> Bids { get; set; }
 	}
 }
